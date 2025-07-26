3133cb8cada2ee4b29b79185f459f22da1a0e882
skibidi_taneesh <158366390+IIVortexll@users.noreply.github.com>
2025-07-25 22:11:40 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+#include "Item.h"
 
 int main() {
+    Item item1("Pencil", 10);
+    Item item2("Pencil", 10);
+    Item item3("Notebook", 5);
+
+    std::cout << item1 << std::endl;
+
+    if (item1 == item2) {
+        std::cout << "item1 is equal to item2" << std::endl;
+    }
+
+    if (!(item1 == item3)) {
+        std::cout << "item1 is not equal to item3" << std::endl;
+    }
+
     return 0;
 }
