ca15a6f861e1ba7ea7cc08f487365a4b0f47db98
skibidi_taneesh <158366390+IIVortexll@users.noreply.github.com>
2025-07-25 22:29:45 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,64 @@
 #include <iostream>
-#include "Item.h"
+#include <vector>
+#include "item.h"
+using namespace std;
 
 int main() {
-    Item item1("Pencil", 10);
-    Item item2("Pencil", 10);
-    Item item3("Notebook", 5);
+    vector<Item> inventory;
+    int choice;
+    string name;
+    int quantity;
 
-    std::cout << item1 << std::endl;
+    do {
+        cout << "\n1) Add Item\n2) Modify Item\n3) Delete Item\n4) Show Items\n5) Exit\n>> ";
+        cin >> choice;
+        cin.ignore();
 
-    if (item1 == item2) {
-        std::cout << "item1 is equal to item2" << std::endl;
-    }
+        if (choice == 1) {
+            cout << "Enter name: ";
+            getline(cin, name);
+            cout << "Enter quantity: ";
+            cin >> quantity;
+            inventory.push_back(Item(name, quantity));
+        }
 
-    if (!(item1 == item3)) {
-        std::cout << "item1 is not equal to item3" << std::endl;
-    }
+        else if (choice == 2) {
+            cout << "Enter name to modify: ";
+            getline(cin, name);
+            bool found = false;
+            for (int i = 0; i < inventory.size(); i++) {
+                if (inventory[i].getName() == name) {
+                    cout << "Enter new quantity: ";
+                    cin >> quantity;
+                    inventory[i].setQuantity(quantity);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) cout << "Item not found\n";
+        }
+
+        else if (choice == 3) {
+            cout << "Enter name to delete: ";
+            getline(cin, name);
+            bool found = false;
+            for (int i = 0; i < inventory.size(); i++) {
+                if (inventory[i].getName() == name) {
+                    inventory.erase(inventory.begin() + i);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) cout << "Item not found\n";
+        }
+
+        else if (choice == 4) {
+            for (int i = 0; i < inventory.size(); i++) {
+                cout << inventory[i] << endl;
+            }
+        }
+
+    } while (choice != 5);
 
     return 0;
 }
