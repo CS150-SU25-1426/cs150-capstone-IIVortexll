403f28b8863607da8ead8bad0295316bc75e801b
skibidi_taneesh <158366390+IIVortexll@users.noreply.github.com>
2025-07-25 22:27:29 -0700

    Update item.h

--- item.h
+++ item.h
@@ -3,19 +3,23 @@
 
 #include <string>
 #include <iostream>
+using namespace std;
 
 class Item {
 private:
-    std::string name;
-    int quantity;
+    string m_name;
+    int m_quantity;
 
 public:
-    Item(std::string n, int q);
-    std::string getName() const;
+    Item(string name = "", int quantity = 0);
+    string getName() const;
     int getQuantity() const;
 
+    void setName(string name);
+    void setQuantity(int quantity);
+
     bool operator==(const Item& other) const;
-    friend std::ostream& operator<<(std::ostream& out, const Item& item);
+    friend ostream& operator<<(ostream& out, const Item& item);
 };
 
 #endif
